232a1538468746c4444d7f21429f4ca5c1ffc118
Gary Cordero Rosa <garycordero1690@gmail.com>
2020-04-20 01:30:51 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array,string)
+  array.push(string)
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array,num_of_elements = 2)
+  array.pop(num_of_elements)
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array, num_of_elements = 2)
+  array.shift(num_of_elements)
+end
